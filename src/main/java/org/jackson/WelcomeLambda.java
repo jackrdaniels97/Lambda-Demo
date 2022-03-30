3ce640294eb5e0f310225d2d026177f33a70383e
Jackson Daniels <jaydigitydawg@gmail.com>
2022-03-29 21:55:45 -0500

    Initial commit

---
+++ src/main/java/org/jackson/WelcomeLambda.java
@@ -0,0 +1,14 @@
+package org.jackson;
+
+/**
+ * What is the best movie ever
+ *
+ */
+public class WelcomeLambda {
+
+    public String BestMovie() {
+        return "Jurassic Park is the best movie ever!!!";
+    }
+
+
+}
